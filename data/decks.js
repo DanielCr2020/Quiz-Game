05709b1bab21d87490f45ae7979a984fd8936555
UnrealOG <creepercraft137@gmail.com>
2022-12-27 18:40:57 -0500

    Decks can now be edited and made public
    Some minor changes to the way xss is handled

--- data/decks.js
+++ data/decks.js
@@ -4,6 +4,11 @@ const userFunctions=require('./users')
 const validation=require('../validation')
 const {ObjectId} = require('mongodb')
 
+function fn(str){       //adds leading 0 to 1 digit time numbers
+    if(str.toString().length===1) str='0'+str.toString();
+    return str
+}
+
 const createDeck = async (creator,deckName,subject,isPublic,cardsArray,dateCreated) => {
     creator=validation.checkUsername(creator)
     deckName=validation.checkDeckName(deckName)
@@ -11,9 +16,10 @@ const createDeck = async (creator,deckName,subject,isPublic,cardsArray,dateCreat
 
     const userCollection = await users();
     const deckCreator=await userCollection.findOne({username:creator.toLowerCase()})
-    const deckList=deckCreator.decks
-    if( (deckList.filter((deck) => {return deck.name.toLowerCase()===deckName.toLowerCase()})).length>0 )    //checks if you already have that deck
-        throw `You already have a deck called ${deckName}`
+    for(deck of deckCreator.decks){
+        if(deck.name.toLowerCase()===deckName.toLowerCase())    //checks if you already have that deck
+            throw `You already have a deck called ${deckName}`
+    }
     if(Array.isArray(cardsArray)) cards=cardsArray
     else cards=[]
     if(typeof dateCreated!='undefined'){
@@ -24,7 +30,7 @@ const createDeck = async (creator,deckName,subject,isPublic,cardsArray,dateCreat
         _id: ObjectId().toString(),
         name:deckName,
         subject:subject,
-        dateCreated: `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`,
+        dateCreated: `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${fn(d.getHours())}:${fn(d.getMinutes())}:${fn(d.getSeconds())}`,
         creator:creator,
         public:isPublic,
         cards:cards
@@ -41,7 +47,7 @@ const getUsersDecks = async(username) => {
     username=validation.checkUsername(username)
     const userCollection=await users();
     const user=userCollection.findOne({username:username})
-    
+    if(!user) throw "Could not get user"
     return user.decks
 }
 
@@ -56,20 +62,45 @@ const deleteDeck = async(creator,deckId) => {
     return userFromDeck
 }
 
-const getDeckById = async(deckId) => {
+const getDeckById = async(username,deckId) => {
     deckId=validation.checkId(deckId)
+    username=validation.checkUsername(username)
     const userCollection=await users()
-    const deckFound=await userCollection.findOne(
-        {"_id": deckId}
+    const deckFoundUser=await userCollection.findOne(
+        {username:username,                     //finds user 
+         decks:{$elemMatch: {_id:deckId}}},     //then finds the deck that elemMatches the deckId
+        {projection: {"decks.$":1}}             //empty projection on the first one(?????)
     )
-    console.dir(deckFound,{depth:null})
+    const deckFound=deckFoundUser.decks[0]
     if(!deckFound) throw new Error("Unable to find that deck")
     return deckFound
+}
+
+const editDeck = async(username,deckId,newName,newSubject,newPublicity) => {
+    deckId=validation.checkId(deckId)
+    username=validation.checkUsername(username)
+    if(newName) newName=validation.checkDeckName(newName)
+    if(newSubject) newSubject=validation.checkSubject(newSubject)       //only check the name and subject if they are provided
+    const userCollection=await users()
+    const deckCreator=await userCollection.findOne({username:username.toLowerCase()})
+    const oldDeck=await getDeckById(username,deckId)        //used for comparison at the end, if anything was actually modified
+    for(deck of deckCreator.decks){         //checks if you already have that deck
+        if(deck.name.toLowerCase()===newName.toLowerCase() && deck._id.toString()!==deckId.toString())  //if name is same but id is different    
+            throw `You already have a deck called ${newName}`
+    }
+    const editedDeck=await userCollection.updateOne(            //updating deck
+        {username:username,"decks._id":deckId.toString()},
+        {$set: {"decks.$.name":newName, "decks.$.subject":newSubject, "decks.$.public":newPublicity}}   //updates specific fields
+    )
+    if(editedDeck.modifiedCount===0 && !(oldDeck.name.toLowerCase()===newName.toLowerCase() && oldDeck.subject.toLowerCase()===newSubject.toLowerCase() && oldDeck.public===newPublicity)) 
+        throw "Could not successfully update deck"
+    return await getDeckById(username,deckId);
 }
 
 module.exports = {
     createDeck,
     getUsersDecks,
     deleteDeck,
-    getDeckById
+    getDeckById,
+    editDeck
 }
